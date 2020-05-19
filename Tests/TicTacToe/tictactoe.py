8a578b345c1a12817bf8e5b34ddabc74d294ac59
nachooman <ignacio.gnc@gmail.com>
2020-05-19 11:37:21 -0300

    changes made

--- Tests/TicTacToe/tictactoe.py
+++ Tests/TicTacToe/tictactoe.py
@@ -1,13 +1,14 @@
 #primer juego. Ta te ti - tic tac toe
-#Libreria para el tictactoe
-import itertools
 
-po1,po2,po3,po4,po5,po6,po7,po8,po9=' ',' ',' ',' ',' ',' ',' ',' ',' '
+#import itertools for the games combination
+import itertools
+from IPython.display import clear_output
+
 movimientos = []
 players = {}
 winner_combinations = (('1','2','3'), ('4','5','6'), ('7','8','9'), ('1','4','7'), ('2','5','8'), ('3','6','9'), ('1','5','9'), ('3','5','7'))
 
-def konwplayer(pla):
+def knowplayer(pla):
     if pla==1:
         return players['player1']
     else:
@@ -15,8 +16,10 @@ def konwplayer(pla):
 
 
 def dibujarTablero(movs=[]):
+    clear_output()
+    po1,po2,po3,po4,po5,po6,po7,po8,po9=' ',' ',' ',' ',' ',' ',' ',' ',' '
     for x in range(0,len(movs)):
-        globals()["po"+str(movs[x][1])] = konwplayer(movs[x][0])
+        locals()["po"+str(movs[x][1])] = knowplayer(movs[x][0])
 
     print('   |   |   ')
     print(f' {po1} | {po2} | {po3} ')
@@ -58,7 +61,6 @@ def verif_winner(player):
 
     combinaciones_tot = itertools.permutations(mov_player1)
     for x in combinaciones_tot:
-        print(x)
         return x in winner_combinations
 
 #________________________________________________________________
@@ -70,18 +72,23 @@ setplayers()
 dibujarTablero()
 
 player=1
-for x in range(1,9):
-    mov_made = int(input(f'Make your move Player {player} = '))
-    if verif_mov(mov_made) == False:
-        movimientos.append((player, mov_made))
-        dibujarTablero(movimientos)
-    
-    if x>4 :
-        if verif_winner(player) == True:
-            print('CONGRATS player '+ player +' is the winner!!!! ')
-            break
+newgame = 'Yes'
+while newgame == 'Yes':
+    newgame=''
+    for x in range(1,9):
+        mov_made = int(input(f'Make your move Player {player} = '))
+        if verif_mov(mov_made) == False:
+            movimientos.append((player, mov_made))
+            dibujarTablero(movimientos)
+        
+        if x>4 :
+            if verif_winner(player) == True:
+                print('CONGRATS player '+ str(player) +' is the winner!!!! ')
+                break
 
-    if player==1:
-        player=2
-    elif player==2:
-        player=1
+        if player==1:
+            player=2
+        elif player==2:
+            player=1
+    
+    newgame = input('would you like to play a new game? Yes or No')
